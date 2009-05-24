360f68b7434f8dfec24db0ebd21749ce47aad0be
Marc Poulhiès <marc@g-miette.(none)>
2009-05-24 10:40:22 +0200

    generate csv file, works nearly fine :)

--- chronodom.py
+++ chronodom.py
@@ -17,11 +17,11 @@ class Chrono(Thread):
         self.start_date = None
 
     def run(self):
-        start_date = datetime.datetime.today()
+        self.start_date = datetime.datetime.today()
 
         while self.please_loop:
             now = datetime.datetime.today()
-            delta = now - start_date
+            delta = now - self.start_date
             gobject.idle_add(self.btn_widget.set_label, str(delta))
             time.sleep(self.delta)
 
@@ -34,36 +34,84 @@ class ChronoDom:
         self.w.show_all()
 
         self.chrono1_btn = self.xml.get_widget("chrono_button1")
-        self.chrono2_btn = self.xml.get_widget("chrono_button2")
-        
         self.chrono1 = None
-        self.chrono2 = None
 
+        self.sw = self.xml.get_widget("scrolledwindow1")
+
+        self.start_btn = self.xml.get_widget("start_button1")
+
+        self.treestore = gtk.TreeStore(str)
+        self.tree = self.xml.get_widget("treeview1")
+        self.tree.set_model(self.treestore)
+
+        self.tree.set_headers_visible(True)
+        renderer = gtk.CellRendererText()
+        column = gtk.TreeViewColumn("Temps",renderer, text=0)
+        column.set_resizable(True)
+        self.tree.append_column(column)
+
+        self.fc = self.xml.get_widget("filechooserdialog1")
         gtk.main()
+
+    def insert_row(self, col, parent=None):
+        myiter = self.treestore.prepend(None)
+        self.treestore.set_value(myiter, 0, col)
+        return myiter
 
     def on_start_button1_clicked(self, widget):
         print "chrono1"
         if self.chrono1 == None:
             self.chrono1 = Chrono(None, btn=self.chrono1_btn)
             self.chrono1.start()
+            self.start_btn.set_label("STOP")
         else:
             self.chrono1.please_loop = False
             self.chrono1 = None
+            self.start_btn.set_label("START")
 
-    def on_start_button2_clicked(self, widget):
-        print "chrono2"
-        if self.chrono2 == None:
-            self.chrono2 = Chrono(None, btn=self.chrono2_btn)
-            self.chrono2.start()
-        else:
-            self.chrono2.please_loop = False
-            self.chrono2 = None
+    
+    def on_save_button_clicked(self, widget):
+        chooser = gtk.FileChooserDialog(
+            title="Save Output",action=gtk.FILE_CHOOSER_ACTION_SAVE,
+            buttons=(gtk.STOCK_CANCEL,gtk.RESPONSE_CANCEL,
+                     gtk.STOCK_SAVE,gtk.RESPONSE_OK))
+        response = chooser.run()
+        filename = chooser.get_filename()
+
+        chooser.destroy()
+        print filename
+
+        treeiter = self.treestore.get_iter_first()
+        if treeiter == None:
+            return
+
+        fout = open(filename, "w")
+
+        while treeiter != None:
+            value = self.treestore.get_value(treeiter, 0)
+            print >>fout, value, ","
+            treeiter = self.treestore.iter_next(treeiter)
+        fout.close()
+
 
     def on_chrono_button1_clicked(self, widget):
         print "start1"
 
-    def on_chrono_button2_clicked(self, widget):
-        print "start2"
+        if self.chrono1 == None:
+            return
+
+        now = datetime.datetime.today()
+        delta = now - self.chrono1.start_date
+
+        self.insert_row(str(delta))
+
+        # adj = self.sw.get_vadjustment()
+        # print "b", adj.get_value()
+
+        # p = adj.lower
+        # adj.set_value(p)
+        # print "a", adj.get_value()
+
 
 if __name__ == '__main__':
     gtk.gdk.threads_init()
